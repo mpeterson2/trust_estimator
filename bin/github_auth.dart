a24fcd171d73ffb09aa2c8442e5c641b61d07081
Michael Peterson <mpeterson418@gmail.com>
2014-07-07 10:25:38 -0400

    Really removed everything.

--- bin/github_auth.dart
+++
@@ -1,8 +0,0 @@
-/** 
- * For more information on how to obtain a code, visit:
- * https://developer.github.com/v3/oauth/
- * 
- * To register the app, visit:
- * https://github.com/settings/applications/new
- */
-final auth = "be134121a1caabdc660680329c132e87f17ba4a8";
